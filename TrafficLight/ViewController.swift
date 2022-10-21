c0147036c5a85a9425f6a407eaca6916eb54a650
Aroslav Kochkin <kochkin.2328@inbox.ru>
2022-10-21 23:08:15 +0700

    configuring elements at startup

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    
+    @IBOutlet weak var colorChangeButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.layer.cornerRadius = redLightView.bounds.width / 2
+        redLightView.alpha = 0.3
+        
+        yellowLightView.layer.cornerRadius = yellowLightView.bounds.width / 2
+        yellowLightView.alpha = 0.3
+        
+        greenLightView.layer.cornerRadius = greenLightView.bounds.width / 2
+        greenLightView.alpha = 0.3
+        
+        colorChangeButton.setTitle("Start", for: .normal)
+        colorChangeButton.backgroundColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
     }
+    
 
-
+    @IBAction func colorChangeButtonTapped() {
+        
+    }
+    
+    
 }
 
